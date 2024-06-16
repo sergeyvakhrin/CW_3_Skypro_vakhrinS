7e7bb2786d51c3c2c1af3d4b085ce7a934229931
sergeyvakhrin <parlamentsv@mail.ru>
2024-06-16 16:52:01 +0300

    add utils func get_category_list()

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,28 @@
 import json
 
+from src.category import Category
+from src.product import Product
 
-def load_data(path):
+
+def load_data(path) -> list[dict]:
+    """ Загрузка данных из файла """
     with open(path, "rt", encoding="UTF-8") as file:
         return json.load(file)
+
+
+def get_category_list(data) -> list:
+    """ Создание экземпляров классов """
+    categories = []
+    for category in data:
+        products = []
+        for product in category["products"]:
+            products.append(Product(**product))
+        category["products"] = products
+        categories.append(Category(**category))
+    return categories
+
+
+
+
+
 
