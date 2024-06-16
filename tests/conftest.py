01d95320ce8a427def4902c8fefd255cd730b088
sergeyvakhrin <parlamentsv@mail.ru>
2024-06-16 18:24:10 +0300

    add fi[ture & test util get_category_list()

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,3 +30,25 @@ def category2():
                     products=[
                         Product("55 QLED 4K", "Фоновая подсветка", 123000.0, 7),
                               ])
+
+
+@pytest.fixture
+def data_json():
+    return [
+    {
+        "name": "Смартфоны",
+        "description": "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни",
+        "products": [  {"name": "Samsung Galaxy C23 Ultra", "description": "256GB, Серый цвет, 200MP камера",
+                        "price": 180000.0,
+                        "quantity": 5},
+                       {"name": "Samsung Galaxy C23 Ultra", "description": "256GB, Серый цвет, 200MP камера",
+                        "price": 180000.0,
+                        "quantity": 5}
+                       ]
+    },
+    {
+        "name": "Телевизоры",
+        "description": "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+        "products": [  {"name": "55 QLED 4K", "description": "Фоновая подсветка", "price": 123000.0, "quantity": 7}]
+    }
+    ]
