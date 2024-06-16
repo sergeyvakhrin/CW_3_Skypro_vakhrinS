37b741b64a47962d6477d22fdef8042ca5140d83
sergeyvakhrin <parlamentsv@mail.ru>
2024-06-16 19:39:59 +0300

    add getter & setter for attr products in class Category

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 class Category:
     """ Класс категорий товаров """
     product_count = 0
@@ -6,6 +9,16 @@ class Category:
     def __init__(self, name: str, description: str, products: list):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+
+    @property
+    def products(self):
+        return self.__products
+
+    @products.setter
+    def products(self, product: Product):
+        self.__products.append(product)
+        Category.product_count += 1
+
